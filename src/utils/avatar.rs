b177ffbaac25860d3609d1dd9b23c3cbcd7b88dd
lumingrui <lomirus@qq.com>
2021-09-05 10:38:44 +0800

    refactor: discard lazy_static

--- src/utils/avatar.rs
+++ src/utils/avatar.rs
@@ -1,9 +1,9 @@
 use crate::utils::table::AVATARS;
 
 pub fn get_avatar(name: &str) -> &str {
-    if let Some(id) = AVATARS.get(name) {
-        return id
+    if let Some(id) = AVATARS.iter().position(|item| item.0 == name) {
+        AVATARS[id].1
     } else {
-        return ""
+        ""
     }
 }
